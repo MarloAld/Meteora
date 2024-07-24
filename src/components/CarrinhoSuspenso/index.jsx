7abc7587b97d74947f13fe10d3fc0f6ff9e57c8d
Marlinton Aldrighi <marlinton13@gmail.com>
2024-07-24 14:59:55 -0300

    Final version

--- src/components/CarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/index.jsx
@@ -4,9 +4,10 @@ import ListaProdutosCarrinho from "@/components/ListaProdutosCarrinho";
 import TotalCarrinho from "./TotalCarrinho";
 import Titulo from "@/components/Titulo";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
 const CarrinhoSuspenso = () => {
-  const {carrinho} = useContext(CarrinhoContext)
+  const { carrinho, valorTotal } = useCarrinhoContext()
   return (
     <div
       className="offcanvas offcanvas-end text-bg-dark"
@@ -30,10 +31,8 @@ const CarrinhoSuspenso = () => {
         ></Botao>
       </div>
       <div className="offcanvas-body">
-        <ListaProdutosCarrinho
-          carrinho={carrinho}
-        />
-        {/* <TotalCarrinho valorTotalCarrinho={valorTotalCarrinho} /> */}
+        <ListaProdutosCarrinho carrinho={carrinho} />
+        <TotalCarrinho valorTotalCarrinho={valorTotal} />
       </div>
     </div>
   );
--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,13 +1,41 @@
-import React, { createContext, useState } from "react";
+import { createContext, useReducer, useState, useEffect, useMemo } from "react";
+import { carrinhoReducer } from "@/reducers/CarrinhoReducer";
 
-export const CarrinhoContext = createContext()
+export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = 'Carrinho'
 
-export const CarrinhoProvider = ({children}) => {
-    const [carrinho, setCarrinho] = useState([])
+const estadoInicial = []
 
-    return (
-        <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
-            {children}
-        </CarrinhoContext.Provider>
+export const CarrinhoProvider = ({ children }) => {
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial)
+  const [quantidade, setQuantidade] = useState(0)
+  const [valorTotal, setValorTotal] = useState(0)
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce((acumulador, produto) => ({
+      quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+      totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+    }),
+      {
+        quantidadeTemp: 0,
+        totalTemp: 0
+      }
     )
-}
+  }, [carrinho])
+
+  useEffect(() => {
+    setQuantidade(quantidadeTemp)
+    setValorTotal(totalTemp)
+  })
+
+  return (
+    <CarrinhoContext.Provider value={{ 
+      carrinho, 
+      dispatch, 
+      quantidade, 
+      valorTotal
+    }}>
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
